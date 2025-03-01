75c92949d415cf1366102f7040f7dc02826ea96c
Ruben Baltazar <ruben.baltazar.balderrama@gmail.com>
2025-02-28 22:39:03 -0400

    llenado de datos en las tablas2

---
+++ turybusuno/Script/entas.Viajero.data2.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO ventas.Viajero (dni_viajero, nombres_viajero, apellidos_viajero, telefono_viajero, direccion_viajero)
+VALUES
+('3453453', 'Airton', 'Goarita', '78885647', 'Calle Falsa 34534');
